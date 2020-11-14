88bece925423eb79f50c27660b525e9d08955544
Jue <me@jue.yt>
2020-11-14 14:13:06 +0100

    mocks/onoff: create mocks for every invidual instance of Gpio

--- __tests__/in.js
+++ __tests__/in.js
@@ -70,52 +70,57 @@ describe('factory', () => {
 
 	test('read value on change', () => {
 		const value = true;
-		mockOnoff.Gpio.prototype.read.mockImplementationOnce((cb) => cb(null, value ? 1 : 0));
 		const output = {};
 		IN.factory({gpio: 2, watch: true}, [], [output]);
-		mockOnoff.Gpio.prototype.watch.mock.calls[0][0]();
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		gpio.read.mockImplementationOnce((cb) => cb(null, value ? 1 : 0));
+		gpio.watch.mock.calls[0][0]();
 		expect(output.value).toBe(value);
 	});
 
 	test('invert values', () => {
 		const invert = true;
 		const value = true;
-		mockOnoff.Gpio.prototype.read.mockImplementationOnce((cb) => cb(null, value ? 0 : 1));
 		const output = {};
 		IN.factory({gpio: 2, watch: true, invert}, [], [output]);
-		mockOnoff.Gpio.prototype.watch.mock.calls[0][0]();
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		gpio.read.mockImplementationOnce((cb) => cb(null, value ? 0 : 1));
+		gpio.watch.mock.calls[0][0]();
 		expect(output.value).toBe(value);
 	});
 
 	test('ignore errors', () => {
-		mockOnoff.Gpio.prototype.read.mockImplementationOnce((cb) => cb(new Error()));
 		const output = {};
 		IN.factory({gpio: 2, watch: true}, [], [output]);
-		mockOnoff.Gpio.prototype.watch.mock.calls[0][0]();
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		gpio.read.mockImplementationOnce((cb) => cb(new Error()));
+		gpio.watch.mock.calls[0][0]();
 		expect(output.value).toBe(undefined);
 	});
 
 	test('read value by time interval', () => {
 		const value = true;
-		mockOnoff.Gpio.prototype.read.mockImplementationOnce((cb) => cb(null, value ? 1 : 0));
 		const interval = 123;
 		const output = {};
 		IN.factory({gpio: 2, interval}, [], [output]);
-		expect(mockOnoff.Gpio.prototype.read.mock.calls.length).toBe(1);
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		gpio.read.mockImplementationOnce((cb) => cb(null, value ? 1 : 0));
+		expect(gpio.read.mock.calls.length).toBe(1);
 		expect(output.value).toBe(value);
 		jest.advanceTimersByTime(interval);
-		expect(mockOnoff.Gpio.prototype.read.mock.calls.length).toBe(2);
+		expect(gpio.read.mock.calls.length).toBe(2);
 	});
 
 	test('unexport on exit', () => {
 		const interval = 123;
 		const exit = IN.factory({gpio: 2, interval}, [], [{}]);
-		expect(mockOnoff.Gpio.prototype.read.mock.calls.length).toBe(1);
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		expect(gpio.read.mock.calls.length).toBe(1);
 		jest.advanceTimersByTime(interval);
-		expect(mockOnoff.Gpio.prototype.read.mock.calls.length).toBe(2);
+		expect(gpio.read.mock.calls.length).toBe(2);
 		exit();
 		jest.advanceTimersByTime(interval);
-		expect(mockOnoff.Gpio.prototype.read.mock.calls.length).toBe(2);
-		expect(mockOnoff.Gpio.prototype.unexport.mock.calls.length).toBe(1);
+		expect(gpio.read.mock.calls.length).toBe(2);
+		expect(gpio.unexport.mock.calls.length).toBe(1);
 	});
 });
