40e3968f880ce9cacd14a21ae441222fe3d21bde
Jue <me@jue.yt>
2020-11-14 15:00:36 +0100

    add switch

---
+++ __tests__/switch.js
@@ -0,0 +1,157 @@
+const {EventEmitter} = require('events');
+
+jest.mock('onoff');
+const mockOnoff = require('onoff');
+
+const {check, factory} = require('../switch.js');
+
+describe('check', () => {
+	test('expect one output pipe', () => {
+		expect(() => check({
+			input: [],
+			output: []
+		})).toThrow('One output must be specified');
+	});
+	test('set defaults', () => {
+		const opts = {
+			input: [],
+			output: ['foo']
+		};
+		check(opts);
+		expect(opts).toMatchObject({
+			toggleEdge: 'falling',
+			toggleDebounce: 10,
+			onEdge: 'falling',
+			onDebounce: 10,
+			offEdge: 'falling',
+			offDebounce: 10,
+			default: false
+		});
+	});
+});
+
+describe('factory', () => {
+	test('set default', () => {
+		const opts = {default: true};
+		const o = {};
+		factory(opts, [], [o]);
+		expect(o.value).toBe(opts.default);
+	});
+
+	test('source: input pipe', () => {
+		const i = new EventEmitter();
+		const o = {};
+		factory({}, [i], [o]);
+		i.emit('update', 123);
+		expect(o.value).toBeUndefined();
+		i.emit('update', true);
+		expect(o.value).toBe(true);
+		i.emit('update', false);
+		expect(o.value).toBe(false);
+	});
+
+	test('source: toggle button', () => {
+		const opts = {
+			toggleGpio: 123,
+			toggleEdge: 'rising',
+			toggleDebounce: 321,
+			default: true
+		};
+		const o = {};
+		const exit = factory(opts, [], [o]);
+		expect(mockOnoff.Gpio.mock.calls[0][0]).toBe(opts.toggleGpio);
+		expect(mockOnoff.Gpio.mock.calls[0][1]).toEqual('in');
+		expect(mockOnoff.Gpio.mock.calls[0][2]).toBe(opts.toggleEdge);
+		expect(mockOnoff.Gpio.mock.calls[0][3].debounceTimeout).toBe(opts.toggleDebounce);
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		expect(o.value).toBe(true);
+		gpio.watch.mock.calls[0][0]();
+		expect(o.value).toBe(false);
+		gpio.watch.mock.calls[0][0]();
+		expect(o.value).toBe(true);
+		exit();
+		expect(gpio.unexport.mock.calls.length).toBe(1);
+	});
+
+	test('source: on button', () => {
+		const opts = {
+			onGpio: 123,
+			onEdge: 'rising',
+			onDebounce: 321,
+			default: false
+		};
+		const o = {};
+		const exit = factory(opts, [], [o]);
+		expect(mockOnoff.Gpio.mock.calls[0][0]).toBe(opts.onGpio);
+		expect(mockOnoff.Gpio.mock.calls[0][1]).toEqual('in');
+		expect(mockOnoff.Gpio.mock.calls[0][2]).toBe(opts.onEdge);
+		expect(mockOnoff.Gpio.mock.calls[0][3].debounceTimeout).toBe(opts.onDebounce);
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		expect(o.value).toBe(false);
+		gpio.watch.mock.calls[0][0]();
+		expect(o.value).toBe(true);
+		gpio.watch.mock.calls[0][0]();
+		expect(o.value).toBe(true);
+		exit();
+		expect(gpio.unexport.mock.calls.length).toBe(1);
+	});
+
+	test('source: off button', () => {
+		const opts = {
+			offGpio: 123,
+			offEdge: 'rising',
+			offDebounce: 321,
+			default: true
+		};
+		const o = {};
+		const exit = factory(opts, [], [o]);
+		expect(mockOnoff.Gpio.mock.calls[0][0]).toBe(opts.offGpio);
+		expect(mockOnoff.Gpio.mock.calls[0][1]).toEqual('in');
+		expect(mockOnoff.Gpio.mock.calls[0][2]).toBe(opts.offEdge);
+		expect(mockOnoff.Gpio.mock.calls[0][3].debounceTimeout).toBe(opts.offDebounce);
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		expect(o.value).toBe(true);
+		gpio.watch.mock.calls[0][0]();
+		expect(o.value).toBe(false);
+		gpio.watch.mock.calls[0][0]();
+		expect(o.value).toBe(false);
+		exit();
+		expect(gpio.unexport.mock.calls.length).toBe(1);
+	});
+
+	test('LED', () => {
+		const i = new EventEmitter();
+		const opts = {
+			ledGpio: 123,
+			ledInvert: false
+		};
+		const exit = factory(opts, [i], [{}]);
+		expect(mockOnoff.Gpio.mock.calls[0][0]).toBe(opts.ledGpio);
+		expect(mockOnoff.Gpio.mock.calls[0][1]).toEqual('out');
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		i.emit('update', true);
+		expect(gpio.writeSync.mock.calls[0][0]).toBe(1);
+		i.emit('update', false);
+		expect(gpio.writeSync.mock.calls[1][0]).toBe(0);
+		exit();
+		expect(gpio.unexport.mock.calls.length).toBe(1);
+	});
+
+	test('LED inverted', () => {
+		const i = new EventEmitter();
+		const opts = {
+			ledGpio: 123,
+			ledInvert: true
+		};
+		const exit = factory(opts, [i], [{}]);
+		expect(mockOnoff.Gpio.mock.calls[0][0]).toBe(opts.ledGpio);
+		expect(mockOnoff.Gpio.mock.calls[0][1]).toEqual('out');
+		const gpio = mockOnoff.Gpio.mock.instances[0];
+		i.emit('update', true);
+		expect(gpio.writeSync.mock.calls[0][0]).toBe(0);
+		i.emit('update', false);
+		expect(gpio.writeSync.mock.calls[1][0]).toBe(1);
+		exit();
+		expect(gpio.unexport.mock.calls.length).toBe(1);
+	});
+});
---
+++ switch.js
@@ -0,0 +1,93 @@
+const {EventEmitter} = require('events');
+const Gpio = require('onoff').Gpio;
+
+function check (opts) {
+	if (opts.output.length !== 1) throw new Error('One output must be specified');
+	if (opts.toggleEdge === undefined) opts.toggleEdge = 'falling';
+	if (opts.onEdge === undefined) opts.onEdge = 'falling';
+	if (opts.offEdge === undefined) opts.offEdge = 'falling';
+	if (opts.toggleDebounce === undefined) opts.toggleDebounce = 10;
+	if (opts.onDebounce === undefined) opts.onDebounce = 10;
+	if (opts.offDebounce === undefined) opts.offDebounce = 10;
+	if (opts.default === undefined) opts.default = false;
+}
+
+class Bool extends EventEmitter {
+	set (newBool) {
+		if (typeof newBool !== 'boolean') return;
+		const oldBool = this._value;
+		this._value = newBool;
+		if (newBool !== oldBool) this.emit('change', newBool);
+	}
+	get () {
+		return this._value;
+	}
+}
+
+function factory (opts, input, output) {
+	const onState = new Bool();
+	const exportedGpios = [];
+
+	// Wire up soruces:
+	// - input pipe
+	if (input[0]) {
+		input[0].on('update', (value) => {
+			onState.set(value);
+		});
+	}
+	// - toggle button
+	if (opts.toggleGpio !== undefined) {
+		const toggleGpio = new Gpio(
+			opts.toggleGpio,
+			'in',
+			opts.toggleEdge,
+			{debounceTimeout: opts.toggleDebounce}
+		);
+		toggleGpio.watch(() => onState.set(!onState.get()));
+		exportedGpios.push(toggleGpio);
+	}
+	// - on button
+	if (opts.onGpio !== undefined) {
+		const onGpio = new Gpio(
+			opts.onGpio,
+			'in',
+			opts.onEdge,
+			{debounceTimeout: opts.onDebounce}
+		);
+		onGpio.watch(() => onState.set(true));
+		exportedGpios.push(onGpio);
+	}
+	// - off button
+	if (opts.offGpio !== undefined) {
+		const offGpio = new Gpio(
+			opts.offGpio,
+			'in',
+			opts.offEdge,
+			{debounceTimeout: opts.offDebounce}
+		);
+		offGpio.watch(() => onState.set(false));
+		exportedGpios.push(offGpio);
+	}
+
+	// Wire up sinks:
+	// - output pipe
+	onState.on('change', (on) => {
+		output[0].value = on;
+	});
+	// - LED
+	if (opts.ledGpio) {
+		const ledGpio = new Gpio(opts.ledGpio, 'out');
+		onState.on('change', (on) => {
+			if (opts.ledInvert) on = !on;
+			ledGpio.writeSync(on ? 1 : 0);
+		});
+		exportedGpios.push(ledGpio);
+	}
+
+	// Set default
+	onState.set(opts.default);
+
+	return () => exportedGpios.forEach((gpio) => gpio.unexport());
+}
+
+module.exports = {check, factory};
